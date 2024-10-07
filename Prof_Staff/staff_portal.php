235d698b5f846e45ca67a609b22ec44b3e491965
Mad-tech-an <tiwa0050@flinders.edu.au>
2024-10-07 17:56:20 +1030

    style

--- Prof_Staff/staff_portal.php
+++ Prof_Staff/staff_portal.php
@@ -14,6 +14,7 @@ if (!isset($_SESSION['loggedin'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="group-27">
+    <link rel ="stylesheet" href="Prof_Staff/style.css">
     <title>Staff Portal | CareWeb Service</title>
 </head>
 <body>
