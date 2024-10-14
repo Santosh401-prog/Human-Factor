fbb80b8d3a5cb35a7f6d539a7c42fb2d0f705a7e
Mad-tech-an <tiwa0050@flinders.edu.au>
2024-10-14 12:27:47 +1030

    php

--- php/therapist_data.php
+++ php/therapist_data.php
@@ -1,5 +1,5 @@
 <?php
-// Fetch available therapists
+// Fetch available therapists to schedule appointments ; this is for staff portal
 $pdo = new PDO('mysql:host=localhost;dbname=care_system', 'root', '');
 $stmt = $pdo->query("SELECT id, name FROM therapists");
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
